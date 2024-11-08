fd552f5c0cc7ed0cabcfc9eaa21cfacccd7cc606
sese2204 <sese2204@naver.com>
2024-11-08 23:38:50 +0900

    feat(domain): 상품 이름, 가격, 수량 관리를 위한 Product 클래스 구현

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,13 @@
+package store.domain;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private final int quantity;
+
+    public Product(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
