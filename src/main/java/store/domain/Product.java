5d6a76eaafc8e7f770362b0c3ad932d8421e5b4e
sese2204 <sese2204@naver.com>
2024-11-08 23:41:59 +0900

    refactor(domain): 프로모션/일반 재고 분리

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -3,11 +3,13 @@ package store.domain;
 public class Product {
     private final String name;
     private final int price;
-    private final int quantity;
+    private final int promotionQuantity;
+    private final int normalQuantity;
 
-    public Product(String name, int price, int quantity) {
+    public Product(String name, int price, int promotionQuantity, int normalQuantity) {
         this.name = name;
         this.price = price;
-        this.quantity = quantity;
+        this.promotionQuantity = promotionQuantity;
+        this.normalQuantity = normalQuantity;
     }
 }
